8825ea598acb0599c510f7b9e257f679a9610362
Quentin <fle.quentin@gmail.com>
2019-09-18 20:26:38 +0200

    Show The Data On Angular Frontend

--- api/DB.js
+++ api/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb+srv://admin:admin@crud-angular-aomyl.mongodb.net/test?retryWrites=true&w=majoritymongodb://localhost:27017/ng8crud'
+    DB: 'mongodb+srv://admin:admin@crud-angular-aomyl.mongodb.net/test?retryWrites=true&w=majority'
   };
